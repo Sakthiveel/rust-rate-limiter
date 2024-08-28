bf9f89716f67c43f19a109bbff1fe35d118e4f2d
Sakthivel <126545453+Sakthiveel@users.noreply.github.com>
2024-08-28 12:59:56 +0000

    improved rate-liming handling logic overalll

--- src/main.rs
+++ src/main.rs
@@ -8,43 +8,61 @@ use std::net::{TcpListener, TcpStream};
 
 #[derive(Debug)]
 struct InMemoryStorage {
-    limit: i32,
-    interval: i64,
+    // limit: i32,
+    // interval: i64,
     memory: HashMap<String, Record>,
 }
 impl InMemoryStorage {
-    pub fn new(limit: i32, interval: i64) -> InMemoryStorage {
+    pub fn new() -> InMemoryStorage {
         InMemoryStorage {
-            limit,
-            interval,
+            // limit,
+            // interval,
             memory: HashMap::new(),
         }
     }
 
-    pub fn consume(&mut self, ip: String) -> bool {
-        println!("IP addres :{}", ip);
-        println!("memory : {:?}", self.memory);
+    pub fn consume(&mut self, ip: String, limit: i32, reset_time: f64, interval: i64) -> bool {
+        // println!("IP addres :{}", ip);
+        // println!("memory : {:?}", self.memory);
         if !self.memory.contains_key(&ip) {
-            self.memory.insert(ip.clone(), Record::new(1, Utc::now()));
+            self.memory.insert(
+                ip.clone(),
+                Record::new(limit, 1, Utc::now(), reset_time, interval),
+            );
             return true;
         }
 
         let cur_record: &mut Record = self.memory.get_mut(&ip).unwrap();
-        let time_delta: TimeDelta = Utc::now() - cur_record.get_first_req_time();
+        let time_delta: TimeDelta = Utc::now() - cur_record.get_last_req_time();
         let seconds_diff: i64 = time_delta.num_seconds();
-        println!("Time diff {:?}", seconds_diff);
-        println!("Record:{:?}", cur_record);
+        // println!("Time diff {:?}", seconds_diff);
         let mut should_allow = false;
-        if cur_record.get_cur_req_count() < self.limit {
+        if seconds_diff >= cur_record.get_interval()
+            && cur_record.get_cur_req_count() < cur_record.get_limit()
+        {
             cur_record.increment_cur_req_count();
-            println!("After increment {:?}", cur_record);
-            println!("*****************************************************************");
+            cur_record.update_last_req_time();
+            // println!("After increment {:?}", cur_record);
+            // println!("*****************************************************************");
             should_allow = true;
         }
-        if seconds_diff > self.interval {
-            self.memory.remove(&ip);
-            println!("IP address blocked");
+        let time_delta: TimeDelta = Utc::now() - cur_record.get_first_req_time();
+        let total_time_diff: f64 = time_delta.num_seconds() as f64;
+        println!(
+            "Diff = {} ,reset_time = {} ",
+            total_time_diff,
+            cur_record.get_reset_time() * 60.0
+        );
+        if total_time_diff >= cur_record.get_reset_time() * 60.0 {
             should_allow = false;
+            if seconds_diff >= cur_record.get_interval() {
+                self.memory.remove(&ip);
+                self.memory.insert(
+                    ip.clone(),
+                    Record::new(limit, 1, Utc::now(), reset_time, interval),
+                );
+                should_allow = true;
+            }
         }
 
         return should_allow;
@@ -55,10 +73,10 @@ fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4000")?;
     println!("Server running at {}", listener.local_addr()?);
     // accept connections and process them serially
-    let mut rate_limiter = InMemoryStorage::new(3, 10);
+    let mut rate_limiter = InMemoryStorage::new();
     for stream in listener.incoming() {
         let ip_addr = stream?.peer_addr()?.ip().to_string();
-        let res = rate_limiter.consume(ip_addr);
+        let res = rate_limiter.consume(ip_addr, 4, 0.2, 3);
         if res {
             println!("Allow");
         } else {
@@ -105,3 +123,6 @@ fn main() -> std::io::Result<()> {
 //         }
 //     }
 // }
+// resetTime : duration of time where the interval will be resetted.
+// interval: only one request per this time cycle will be allowed.
+// limit: no of req which will be allowed at the above mentioned interval within the reset_time
--- src/record.rs
+++ src/record.rs
@@ -1,15 +1,29 @@
 use chrono::{DateTime, Utc};
 #[derive(Debug)]
 pub struct Record {
+    limit: i32,
     cur_req_count: i32,
     first_req_time: DateTime<Utc>,
+    last_req_time: DateTime<Utc>,
+    reset_time: f64,
+    interval: i64, // in seconds
 }
 
 impl Record {
-    pub fn new(cur_req_count: i32, first_req_time: DateTime<Utc>) -> Record {
+    pub fn new(
+        limit: i32,
+        cur_req_count: i32,
+        first_req_time: DateTime<Utc>,
+        reset_time: f64, // in mins
+        interval: i64,   // in secs
+    ) -> Record {
         Record {
+            limit,
             cur_req_count,
             first_req_time,
+            last_req_time: first_req_time.clone(),
+            reset_time,
+            interval,
         }
     }
     pub fn get_cur_req_count(&self) -> i32 {
@@ -20,7 +34,26 @@ impl Record {
         self.cur_req_count = self.cur_req_count + 1;
     }
 
-    pub fn get_first_req_time(&self) -> DateTime<Utc> {
-        return self.first_req_time;
+    pub fn get_last_req_time(&self) -> DateTime<Utc> {
+        self.last_req_time
+    }
+
+    pub fn update_last_req_time(&mut self) {
+        self.last_req_time = Utc::now();
+    }
+
+    pub fn get_first_req_time(&mut self) -> DateTime<Utc> {
+        self.first_req_time
+    }
+
+    pub fn get_reset_time(&mut self) -> f64 {
+        self.reset_time
+    }
+
+    pub fn get_interval(&mut self) -> i64 {
+        self.interval
+    }
+    pub fn get_limit(&mut self) -> i32 {
+        self.limit
     }
 }
