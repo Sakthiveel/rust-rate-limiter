f8d8291cd7da4c469f5de4d6d228f6f00bce474d
Sakthivel <126545453+Sakthiveel@users.noreply.github.com>
2024-08-31 11:06:35 +0000

    chore:

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,6 @@ impl InMemoryStorage {
     }
 
     pub fn consume(&mut self, ip: String, limit: i32, reset_time: f64, interval: i64) -> bool {
-        // println!("IP addres :{}", ip);
-        // println!("memory : {:?}", self.memory);
         if !self.memory.contains_key(&ip) {
             self.memory.insert(
                 ip.clone(),
@@ -35,15 +33,12 @@ impl InMemoryStorage {
         let cur_record: &mut Record = self.memory.get_mut(&ip).unwrap();
         let time_delta: TimeDelta = Utc::now() - cur_record.get_last_req_time();
         let seconds_diff: i64 = time_delta.num_seconds();
-        // println!("Time diff {:?}", seconds_diff);
         let mut should_allow = false;
         if seconds_diff >= cur_record.get_interval()
             && cur_record.get_cur_req_count() < cur_record.get_limit()
         {
             cur_record.increment_cur_req_count();
             cur_record.update_last_req_time();
-            // println!("After increment {:?}", cur_record);
-            // println!("*****************************************************************");
             should_allow = true;
         }
         let time_delta: TimeDelta = Utc::now() - cur_record.get_first_req_time();
@@ -87,42 +82,3 @@ fn main() -> std::io::Result<()> {
 
     Ok(())
 }
-
-// fn handle_client(mut stream: TcpStream) {
-//     let mut buffer = [0; 1024]; // Create a buffer to hold the incoming data
-//     if let Ok(addr) = stream.peer_addr() {
-//         println!("Client IP Address: {}", addr.ip());
-//     } else {
-//         println!("Could not get client IP address.");
-//     }
-//     match stream.read(&mut buffer) {
-//         Ok(_) => {
-//             // Convert the buffer to a string
-//             let request = String::from_utf8_lossy(&buffer[..]);
-//
-//             // Split the request by lines and get the first line
-//             let request_line = request.lines().next().unwrap_or("");
-//
-//             // Split the request line by spaces and extract the method
-//             let method = request_line.split_whitespace().next().unwrap_or("");
-//
-//             // Print the method (GET, POST, etc.)
-//             let splitStr: Vec<&str> = request_line.split(" ").collect();
-//             // println!("Req : {:?}", request);
-//
-//             // println!("DATA {request}");
-//             // Optionally, respond to the client
-//             let response = "HTTP/1.1 200 OK\r\n\r\nHello, client!";
-//             stream
-//                 .write_all(response.as_bytes())
-//                 .expect("Failed to write response");
-//             stream.flush().expect("Failed to flush stream");
-//         }
-//         Err(e) => {
-//             eprintln!("Failed to read from the stream: {}", e);
-//         }
-//     }
-// }
-// resetTime : duration of time where the interval will be resetted.
-// interval: only one request per this time cycle will be allowed.
-// limit: no of req which will be allowed at the above mentioned interval within the reset_time
